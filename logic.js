254eed2f9fa8b280a10ab9c333bb4b2664a03228
Nikolay <81419030+Nikolay1580@users.noreply.github.com>
2022-06-07 21:14:57 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,1 +1,17 @@
-console.log(window.screen.width);
+function checkPassword() {
+    var name =  document.getElementById("name").value;
+    var password =  document.getElementById("password").value;
+    console.log(name.value="");
+    if (name.toLowerCase() == "beta" && password.toLowerCase() == "nik") {
+        window.open("video.html", '_blank');
+        
+    } else {
+        document.getElementById("form-container").classList.add("form-input-animation");
+        setTimeout(() => {
+            document.getElementById("form-container").classList.remove("form-input-animation");
+        }, 1000);
+    }
+    document.getElementById("name").value = "";
+    password =  document.getElementById("password").value = "";
+}
+
